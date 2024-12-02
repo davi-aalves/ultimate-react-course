08223d1629927d371bfa614c51bf1be305cfb123
Davi A Alves <daviih2000@gmail.com>
2024-12-02 17:24:10 -0300

    feat(script.js): add logical operators and nullish coalescing examples

--- js-review/script.js
+++ js-review/script.js
@@ -182,11 +182,34 @@ updatedBook;
 const getYear = (str) => str.split("-")[0];
 console.log(getYear(publicationDate));
 
-const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
-  getYear(publicationDate)
-}. The book has ${hasMovieAdaptation ? "" : "not"}been adapted as a movie.`;
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"}been adapted as a movie.`;
 summary;
 
 const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
 pagesRange;
 console.log(`The book has ${pagesRange} pages.`);
+
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy = 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || 'no data'
+countWrong;
+
+const count = book.reviews.librarything.reviewsCount ?? "no data"
+count
